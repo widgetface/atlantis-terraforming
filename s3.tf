227d3124badd0f987f98c7a55e0be4a64fd5030c
Groggie <groggy67@yahoo.com>
2025-05-19 14:20:25 +0100

    Commit first branch

---
+++ s3.tf
@@ -0,0 +1,24 @@
+# Define the S3 bucket resource
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "sdds-test-bucket-eu-west-2"  # Ensure the bucket name is globally unique
+
+  # Optional: Enable versioning on the S3 bucket
+  versioning {
+    enabled = true
+  }
+
+  # Optional: Define tags for the S3 bucket
+  tags = {
+    Name        = "test-bucket"
+    Environment = "Development"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "app" {
+  bucket = aws_s3_bucket.test_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
