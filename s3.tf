7ce7cb267770544a53cdf84737e3d1f13d2bafb7
Groggie <groggy67@yahoo.com>
2025-05-21 12:42:34 +0100

    Commit cross account provider and test S3 bucket

--- s3.tf
+++ s3.tf
@@ -2,32 +2,32 @@ resource "random_id" "suffix" {
   byte_length = 4
 }
 
-# resource "aws_s3_bucket" "main" {
-#   bucket = "my-secure-bucket-${random_id.suffix.hex}"
-#   force_destroy = false  # set to true only if you want to allow auto-deletion of objects
-#   tags = {
-#     Environment = "dev"
-#     ManagedBy   = "Terraform"
-#   }
+resource "aws_s3_bucket" "main" {
+  bucket = "my-secure-cross-account-bucket-${random_id.suffix.hex}"
+  force_destroy = false  # set to true only if you want to allow auto-deletion of objects
+  tags = {
+    Environment = "dev"
+    ManagedBy   = "Terraform"
+  }
 
-#   lifecycle {
-#     prevent_destroy = true  # Adds protection against accidental deletion
-#   }
-# }
+  lifecycle {
+    prevent_destroy = true  # Adds protection against accidental deletion
+  }
+}
 
-# resource "aws_s3_bucket_versioning" "main" {
-#   bucket = aws_s3_bucket.main.id
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = aws_s3_bucket.main.id
 
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
-# resource "aws_s3_bucket_public_access_block" "main" {
-#   bucket = aws_s3_bucket.main.id
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
 
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
